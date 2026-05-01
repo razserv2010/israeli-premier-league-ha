0623b3c74de328d6b2ecaeaed3edd560321398a0
razserv2010 <47921926+razserv2010@users.noreply.github.com>
2026-05-01 10:03:35 +0300

    Create const.py

---
+++ custom_components/israeli_premier_league/const.py
@@ -0,0 +1,26 @@
+DOMAIN = "israeli_premier_league"
+
+API_BASE_URL = "https://v3.football.api-sports.io"
+LEAGUE_ID = 271
+SEASON = 2025
+
+DEFAULT_SCAN_INTERVAL = 3
+DAYS_AHEAD = 3
+
+DEFAULT_API_KEY = "8b064ef8a37d4f67ffab3e380f382b0b"
+CONF_API_KEY = "api_key"
+CONF_SCAN_INTERVAL = "scan_interval"
+
+ATTR_HOME_TEAM = "home_team"
+ATTR_AWAY_TEAM = "away_team"
+ATTR_HOME_LOGO = "home_logo"
+ATTR_AWAY_LOGO = "away_logo"
+ATTR_MATCH_DATE = "match_date"
+ATTR_MATCH_TIME = "match_time"
+ATTR_STATUS = "status"
+ATTR_VENUE = "venue"
+ATTR_CHANNELS = "channels"
+ATTR_FIXTURE_ID = "fixture_id"
+ATTR_ROUND = "round"
+ATTR_HOME_SCORE = "home_score"
+ATTR_AWAY_SCORE = "away_score"
